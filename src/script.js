7dfaea9a624b428e08dc7719945554ce1a200fcd
mikeanthony23 <mikeanthony595@gmail.com>
2021-06-18 01:14:01 +0800

    styled reviews and services section

--- src/script.js
+++ src/script.js
@@ -1,16 +1,29 @@
-'use strict';
-
 const burgerMenu = document.querySelector('.burger-menu');
 const header = document.querySelector('.header');
 const showMenu = document.querySelector('.show-burger-menu');
+const overlayHidden = document.querySelector('.overlay-hidden');
+const showOverlay = document.querySelector('.overlay-show');
+
+const addOpacity = function () {
+  showMenu.classList.contains('hidden')
+    ? (burgerMenu.style.opacity = 1)
+    : (burgerMenu.style.opacity = 0.5);
+};
 
 burgerMenu.addEventListener('click', function (e) {
   const btn = e.target.closest('.menu');
   if (!btn) return;
-
   showMenu.classList.toggle('hidden');
+  overlayHidden.classList.add('overlay-show');
+  if (showMenu.classList.contains('hidden'))
+    overlayHidden.classList.toggle('overlay-show');
+  addOpacity();
+});
 
-  showMenu.classList.contains('hidden')
-    ? (burgerMenu.style.opacity = 1)
-    : (burgerMenu.style.opacity = 0.5);
+overlayHidden.addEventListener('click', function (e) {
+  const btn = e.target;
+  if (!btn) return;
+  if (overlayHidden.classList.contains('overlay-show'))
+    showMenu.classList.toggle('hidden');
+  addOpacity();
 });
