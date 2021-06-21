c8955a194f4a850f41a74ed8185bd74a415cafcc
mikeanthony23 <mikeanthony595@gmail.com>
2021-06-21 15:51:39 +0800

    refactored

--- src/script.js
+++ src/script.js
@@ -1,21 +1,23 @@
 'use strict';
 
-// navigation menu selectors
 const burgerMenuBtn = document.querySelector('.burger-btn-container');
 const modalNavLinks = document.querySelector('.nav-links-wrapper');
 const overlay = document.querySelector('.overlay');
 
-// navigation menu functionality
 const controlOpacity = function () {
   overlay.classList.contains('show-overlay')
     ? (burgerMenuBtn.style.opacity = 0.5)
     : (burgerMenuBtn.style.opacity = 1);
 };
 
+const toggle = function (className1, className2) {
+  overlay.classList.toggle(className1);
+  modalNavLinks.classList.toggle(className2);
+};
+
 const openBurgerMenu = function () {
   burgerMenuBtn.addEventListener('click', function () {
-    overlay.classList.toggle('show-overlay');
-    modalNavLinks.classList.toggle('hidden');
+    toggle('show-overlay', 'hidden');
     controlOpacity();
   });
 };
@@ -23,8 +25,7 @@ openBurgerMenu();
 
 const closeNavLinks = function () {
   overlay.addEventListener('click', function () {
-    modalNavLinks.classList.toggle('hidden');
-    overlay.classList.toggle('show-overlay');
+    toggle('show-overlay', 'hidden');
     controlOpacity();
   });
 };
